c1176c5a1d947be335ecad1b1544e2830373e430
Rikita Roy <rikitaroy21@gmail.com>
2026-01-29 01:13:08 +0530

    Adding essentials

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,33 +1,47 @@
 
-import Index from "./pages/Index.tsx";
-import SignUp from "./pages/SignUp.tsx";
-import Login from "./pages/Login.tsx";
-import Medicare from "./pages/Medicare.tsx";
-import PathoLab from "./pages/PathoLab.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import Doctors from "./pages/Doctors.tsx";
-import Hospital from "./pages/Hospital.tsx";
-import Ambulance from "./pages/Ambulance.tsx";
-import Support from "./pages/Support.tsx";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
+import Index from "./pages/Index";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Medicare from "./pages/Medicare";
+import PathoLab from "./pages/PathoLab";
+import NotFound from "./pages/NotFound";
+import Doctors from "./pages/Doctors";
+import Hospital from "./pages/Hospital";
+import Ambulance from "./pages/Ambulance";
+import Support from "./pages/Support";
 
-import { Routes, Route } from 'react-router-dom';
-import React from 'react'
+const queryClient = new QueryClient();
 
-const App = () => {
-  <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="/signup" element={<SignUp />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/medicare" element={<Medicare />} />
-    <Route path="/pathocare" element={<PathoLab />} />
-    <Route path="/doctors" element={<Doctors />} />
-    <Route path="/hospital" element={<Hospital />} />
-    <Route path="/ambulance" element={<Ambulance />} />
-    <Route path="/support" element={<Support />} />
-    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/medicare" element={<Medicare />} />
+            <Route path="/pathocare" element={<PathoLab />} />
+            <Route path="/doctors" element={<Doctors />} />
+            <Route path="/hospital" element={<Hospital />} />
+            <Route path="/ambulance" element={<Ambulance />} />
+            <Route path="/support" element={<Support />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
 
-export default App
-
+export default App;
